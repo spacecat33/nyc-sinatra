ba00debbfe131e914832c1407afa37d3bcf135fa
Janan Akkad <jananakkad@hotmail.co.uk>
2022-10-23 15:26:23 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ require 'rack-flash'
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   enable :sessions
-  set :session_secret, ENV{"SESSION_SECRET"}
+  set :session_secret, ENV["SESSION_SECRET"]
   set :views, Proc.new { File.join(root, "../views/") }
   use Rack::Flash
 
